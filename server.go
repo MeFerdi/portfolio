2244450df18676622be52dcc3bb99dc51e8ce48e
MeFerdi <ferdinandodhis254@gmail.com>
2024-11-14 14:41:49 +0300

    Chore: added a skill in skill page

--- server.go
+++ server.go
@@ -1,41 +1,35 @@
-package main
+// package main
 
-import (
-	"log"
-	"net/http"
-	"text/template"
-)
+// import (
+// 	"log"
+// 	"net/http"
+// 	"path"
+// 	"text/template"
+// )
 
-var templates = template.Must(template.ParseGlob("./templates/*.html"))
+// var templates = template.Must(template.ParseGlob("./templates/*.html"))
 
-func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
-	err := templates.ExecuteTemplate(w, tmpl, data)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
+// func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
+// 	err := templates.ExecuteTemplate(w, tmpl, data)
+// 	if err != nil {
+// 		http.Error(w, "Page not found", http.StatusNotFound)
+// 	}
+// }
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		renderTemplate(w, "index.html", nil)
-	})
-	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
-		renderTemplate(w, "about.html", nil)
-	})
-	http.HandleFunc("/blog", func(w http.ResponseWriter, r *http.Request) {
-		renderTemplate(w, "blog.html", nil)
-	})
-	http.HandleFunc("/skills", func(w http.ResponseWriter, r *http.Request) {
-		renderTemplate(w, "skills.html", nil)
-	})
-	http.HandleFunc("/contact", func(w http.ResponseWriter, r *http.Request) {
-		renderTemplate(w, "contact.html", nil)
-	})
+// func main() {
+// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+// 		requestedPage := path.Base(r.URL.Path)
+// 		if requestedPage == "/" || requestedPage == "" {
+// 			requestedPage = "index" // Default to index if root or empty path
+// 		}
+// 		log.Printf("Serving %s page", requestedPage)
+// 		renderTemplate(w, requestedPage+".html", nil)
+// 	})
 
-	// Serve static files for CSS, JS, images
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+// 	// Serve static files for CSS, JS, images
+// 	fs := http.FileServer(http.Dir("static"))
+// 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	log.Println("Server running on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
+// 	log.Println("Server running on http://localhost:8080")
+// 	log.Fatal(http.ListenAndServe(":8080", nil))
+// }
