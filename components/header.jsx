e3269ab8e73bb71823c0a1fff99c04103d51b1a6
MeFerdi <ferdinandodhis254@gmail.com>
2025-07-04 10:41:34 +0300

    feat: add theme toggle to header component

--- components/header.jsx
+++ components/header.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Navigation from "./navigation";
 import Socials from "./socials";
+import ThemeToggle from "./theme-toggle";
 
 const Header = (props) => {
     return (
         <div className='lg:fixed h-screen basis-1.5/4 flex flex-col justify-between pb-48 self-center lg:self-auto'>
             {/* Profile Header */}
             <div className='space-y-6'>
+                {/* Top row with theme toggle */}
+                <div className="flex justify-end mb-4 lg:mb-6">
+                    <ThemeToggle />
+                </div>
+                
                 {/* Avatar/Profile Section */}
                 <div className="flex items-center gap-4 mb-6 lg:flex-col lg:items-start lg:gap-4">
                     {/* Name and Title */}
