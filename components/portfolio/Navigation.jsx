f8922abe0aae86369fd84866a3dcc1ed225fdb3f
MeFerdi <ferdinandodhis254@gmail.com>
2026-03-11 17:44:52 +0300

    feat: add Navigation component for site navigation links

---
+++ components/portfolio/Navigation.jsx
@@ -0,0 +1,17 @@
+export default function Navigation() {
+  return (
+    <nav>
+      <div className="wrap">
+        <span className="nav-logo">FO</span>
+        <ul className="nav-links">
+          <li><a href="#about">about</a></li>
+          <li><a href="#work">work</a></li>
+          <li><a href="#projects">projects</a></li>
+          <li><a href="#writing">writing</a></li>
+          <li><a href="/brand">random stuff</a></li>
+          <li><a href="#contact">contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
