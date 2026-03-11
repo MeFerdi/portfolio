3112cc82ee0a0386c41086a6a30fd97b446b6a6c
MeFerdi <ferdinandodhis254@gmail.com>
2026-03-11 17:45:15 +0300

    feat: create PortfolioPage component to structure the portfolio layout

---
+++ components/portfolio/PortfolioPage.jsx
@@ -0,0 +1,37 @@
+import PortfolioStyles from './PortfolioStyles';
+import Navigation from './Navigation';
+import HeroSection from './HeroSection';
+import AboutSection from './AboutSection';
+import ExperienceSection from './ExperienceSection';
+import ProjectsSection from './ProjectsSection';
+import WritingSection from './WritingSection';
+import ContactSection from './ContactSection';
+import PortfolioFooter from './PortfolioFooter';
+
+export default function PortfolioPage() {
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,400;0,500;0,600;1,400&family=Special+Elite&display=swap"
+        rel="stylesheet"
+      />
+
+      <main className="portfolio-page">
+        <PortfolioStyles />
+        <div className="glitch-sweep" />
+        <div className="glitch-sweep" />
+        <div className="glitch-sweep" />
+        <Navigation />
+        <HeroSection />
+        <AboutSection />
+        <ExperienceSection />
+        <ProjectsSection />
+        <WritingSection />
+        <ContactSection />
+        <PortfolioFooter />
+      </main>
+    </>
+  );
+}
